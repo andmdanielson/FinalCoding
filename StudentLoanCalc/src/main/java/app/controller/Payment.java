65ac880ecac0abcc05b2bd68a2efa963b98550de
amdan <amdan@128.4.145.148>
2019-05-21 15:21:39 -0400

    payment finished

--- StudentLoanCalc/src/main/java/app/controller/Payment.java
+++ StudentLoanCalc/src/main/java/app/controller/Payment.java
@@ -1,15 +1,55 @@
 package app.controller;
 
 import java.time.LocalDate;
+import org.apache.poi.ss.formula.functions.*;
 
 public class Payment {
 	
+	private int num;
 	private double dPayment;
+	private double dAdd;
 	private double dInterest;
+	private double dPrincipal;
+	private double dBalance;
 	private LocalDate date;
 	
-	public Payment(double principal, double rate, double time, LocalDate payDate) {
+	public Payment(int n, double loan, double rate, double time, double additional, double balance, LocalDate payDate) {
+		num=n;
 		date=payDate;
+		double amount = Math.abs(FinanceLib.pmt(rate, time, loan, 0, true));
+		dPayment = amount+additional;
+		dAdd=additional;
+		dInterest=rate*balance;
+		dPrincipal=amount-dInterest;
+		dBalance=balance-dPrincipal;
 		
+	}
+	
+	public int getNumber() {
+		return num;
+	}
+	
+	public LocalDate getDate() {
+		return date;
+	}
+	
+	public double getPayment() {
+		return dPayment;
+	}
+	
+	public double getAdditional() {
+		return dAdd;
+	}
+	
+	public double getInterest() {
+		return dInterest;
+	}
+	
+	public double getPrincipal() {
+		return dPrincipal;
+	}
+	
+	public double getBalance() {
+		return dBalance;
 	}
 }
